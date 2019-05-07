8fec29df7c2be1c65e0462c65969cc4d8b80c12b
Steinmetz Lab User <none@steinmetzlab.net>
2019-05-06 19:32:43 -0500

    update paths for steinmetz lab

--- +dat/paths.m
+++ +dat/paths.m
@@ -13,10 +13,8 @@ if nargin < 1 || isempty(rig)
   rig = thishost;
 end
 
-server1Name = '\\zubjects.cortexlab.net';
-server2Name = '\\zserver.cortexlab.net';
-basketName = '\\basket.cortexlab.net'; % for working analyses
-lugaroName = '\\lugaro.cortexlab.net'; % for tape backup
+server1Name = '\\steinmetzsuper1.biostr.washington.edu\data\';
+
 
 %% defaults
 % path containing rigbox config folders
@@ -25,7 +23,7 @@ p.rigbox = fileparts(which('addRigboxPaths'));
 % Repository for local copy of everything generated on this rig
 p.localRepository = 'C:\LocalExpData';
 p.localAlyxQueue = 'C:\localAlyxQueue';
-p.databaseURL = 'https://alyx.cortexlab.net'; % 'https://dev.alyx.internationalbrainlab.org/';
+p.databaseURL = 'https://alyx.steinmetzlab.net'; % 'https://dev.alyx.internationalbrainlab.org/';
 p.gitExe = 'C:\Program Files\Git\cmd\git.exe';
 % Day on which to update code (0 = Everyday, 1 = Sunday, etc.)
 p.updateSchedule = 2;
@@ -37,21 +35,21 @@ p.mainRepository = fullfile(server1Name, 'Subjects');
 % directory for organisation-wide configuration files, for now these should
 % all remain on zserver
 % p.globalConfig = fullfile(p.rigbox, 'config');
-p.globalConfig = fullfile(server2Name, 'Code', 'Rigging', 'config');
+p.globalConfig = fullfile(server1Name, 'Code', 'Rigging', 'config');
 % directory for rig-specific configuration files
 p.rigConfig = fullfile(p.globalConfig, rig);
 % repository for all experiment definitions
-p.expDefinitions = fullfile(server2Name, 'Code', 'Rigging', 'ExpDefinitions');
+p.expDefinitions = fullfile(server1Name, 'Code', 'Rigging', 'ExpDefinitions');
 
 % repository for working analyses that are not meant to be stored
 % permanently
-p.workingAnalysisRepository = fullfile(basketName, 'data');
+p.workingAnalysisRepository = fullfile(server1Name, 'temp');
 
 % for tape backups, first files go here:
-p.tapeStagingRepository = fullfile(lugaroName, 'bigdrive', 'staging'); 
+% p.tapeStagingRepository = fullfile(lugaroName, 'bigdrive', 'staging'); 
 
 % then they go here:
-p.tapeArchiveRepository = fullfile(lugaroName, 'bigdrive', 'toarchive');
+% p.tapeArchiveRepository = fullfile(lugaroName, 'bigdrive', 'toarchive');
 
 
 %% load rig-specific overrides from config file, if any  
